4761e83b37b0590590c46614a266edfb38d2e904
Rohan Palankar <rohanpalankar8@gmail.com>
2023-04-05 12:13:07 +0530

    modify files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import { useState } from 'react';
 import './App.css';
 import NoteContainer from './Components/NoteContainer/NoteContainer';
 import SideBar from './Components/SideBar/SideBar';
+import { useEffect } from 'react';
 
 function App() {
 
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState( JSON.parse(localStorage.getItem('notes')) || [] );
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  },[notes]);
 
   const addNote = (color) => {
     const tempNotes = [...notes];
--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -14,7 +14,7 @@ export default function SideBar(props) {
 
   return (
     <div className="sidebar">
-      <img src={plusIcon} alt="Add" onClick={() => setListOpen(!listOpen)} title='Create new note' />
+      <img src={plusIcon} alt="Add" className='plus-icon' onClick={() => setListOpen(!listOpen)} title='Create new note' />
       <ul className={`sidebar_list ${listOpen ? 'sidebar_list_active' : ''}`}>
         {colors.map((color, index) => (
           <li
